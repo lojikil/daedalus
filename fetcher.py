6515b11dad78ad5c15dbcf997e3db7cc607f9ac6
Stefan Edwards <saedwards.ecc@gmail.com>
2016-12-28 13:32:18 -0500

    refactor, working fetcher => index; just need to fix the reindexing process

--- fetcher.py
+++ fetcher.py
@@ -6,12 +6,16 @@ from readability import Document
 import sys
 from bs4 import BeautifulSoup
 import re
+from TokenStore import TokenStore
 
 if len(sys.argv) < 2:
     print "Usage: fetcher.py <url0> <url1> ... <urlN>"
     sys.exit(0)
 
 slugify = re.compile('[^a-zA-Z0-9]+')
+stopwords = open('stopwords.dat').read().split()
+# need a rehydrate step from the sqlite db...
+ts = TokenStore([], stopwords)
 
 for arg in sys.argv[1:]:
     response = requests.get(arg)
@@ -25,5 +29,10 @@ for arg in sys.argv[1:]:
     tag = soup.new_tag("a", href=arg, contents=site)
     soup.body.insert(0, tag)
     html = soup.prettify("utf-8")
-    with open('./cache/{0}.html'.format(slug), "w") as fh:
+    filename = './cache/{0}.html'.format(slug)
+    ts.tokenize_and_add(filename[2:], text=text, new=True)
+    with open(filename, "w") as fh:
         fh.write(html)
+
+ts.build_index()
+ts.dump_database("./index_test.db")
